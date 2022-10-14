0174a3bf13403004a2e0b4ddcf285de28183fa6a
kasperg3 <kasperg3@rocketmail.com>
2022-10-14 13:09:29 +0200

    added module 7 python file

---
+++ idt_module_7_materials/module7.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+
+
+def main():
+    # TODO 
+    # load csv file test.csv, 
+    # convert from Geodetic to UTM
+    # do outlier detection, based on for example velocity
+    # Remove outliers
+    # convert back to lat lon
+    # Create a mission plan
+    pass
+
+
+if __name__ == '__main__':
+    main()
