b6e3f637fd3939d709ec5465d410dd55a55e5bb0
kasperg3 <kasperg3@rocketmail.com>
2022-10-31 11:05:39 +0100

    fixed path pruning

--- idt_module_7_materials/module7.py
+++ idt_module_7_materials/module7.py
@@ -58,29 +58,30 @@ def window(seq, n=2):
 
 
 # Methods max_dist, max_speed, and statistical
-def filter_outliers(dl,max_speed):
+def filter_outliers(dl, max_speed):
     filtered_data = []
     # if method == 'statistical':
     #     window_size = 30
     #     for i in range(len(dl.data) - window_size + 1):
-    #         sequence = pd.DataFrame(dl.data[i: i + window_size]) 
+    #         sequence = pd.DataFrame(dl.data[i: i + window_size])
     #         easting_z_score = (dl.data[i]['easting'] - sequence['easting'].mean()) / sequence['easting'].std()
     #         northing_z_score = (dl.data[i]['northing'] - sequence['northing'].mean()) / sequence['northing'].std()
-            
+
     #         # remove outliers based on the z score
-    #         if abs(easting_z_score) < 1 or abs(northing_z_score) < 1 : 
+    #         if abs(easting_z_score) < 1 or abs(northing_z_score) < 1 :
     #             filtered_data.append(dl.data[i])
-    # elif method == 'max_speed':   
-    for i in range(len(dl.data) -1):
-        pos_change = math.sqrt((dl.data[i]['easting'] - dl.data[i+1]['easting'])**2 + (dl.data[i]['northing'] - dl.data[i+1]['northing'])**2)
-        time_diff = abs(dl.data[i]['time'] - dl.data[i+1]['time']) # Micro to seconds
+    # elif method == 'max_speed':
+    for i in range(len(dl.data) - 1):
+        pos_change = math.sqrt((dl.data[i]['easting'] - dl.data[i+1]['easting'])**2 + (
+            dl.data[i]['northing'] - dl.data[i+1]['northing'])**2)
+        time_diff = abs(dl.data[i]['time'] - dl.data[i+1]
+                        ['time'])  # Micro to seconds
         if time_diff != 0.0:
             speed = pos_change/(time_diff*0.000001)
-            if(speed < max_speed):
-                filtered_data.append(dl.data[i])        
+            if (speed < max_speed):
+                filtered_data.append(dl.data[i])
 
     return filtered_data
-
 
 
 def DouglasPeucker(data, epsilon=0.01):
@@ -88,30 +89,29 @@ def DouglasPeucker(data, epsilon=0.01):
     # Find the point with the maximum distance
     dmax = 0
     index = 0
-    end = len(data) -1
+    end = len(data) - 1
     for i in range(len(data)-1):
         p1 = numpy.array([data[0]['northing'], data[0]['easting']])
         p2 = numpy.array([data[end]['northing'], data[end]['easting']])
         p3 = numpy.array([data[i]['northing'], data[i]['easting']])
-        d = numpy.linalg.norm(numpy.cross(p2-p1, p1-p3))/numpy.linalg.norm(p2-p1)
-        if d > dmax: 
+        d = numpy.linalg.norm(numpy.cross(p2-p1, p1-p3)) / \
+            numpy.linalg.norm(p2-p1)
+        if d > dmax:
             index = i
             dmax = d
     result_list = []
-    
+
     # # If max distance is greater than epsilon, recursively simplify
-    if(dmax > epsilon):
+    if (dmax > epsilon):
         # Recursive call
-        recResults1 = DouglasPeucker(data[0:index+1], epsilon)
+        recResults1 = DouglasPeucker(data[0:index], epsilon)
         recResults2 = DouglasPeucker(data[index:end], epsilon)
-    
+
         # Build the result list
         result_list = recResults1 + recResults2
-    else: 
-        result_list = data
+    else:
+        result_list = [data[0], data[end]]
     return result_list
-    
-    
 
 
 def main():
@@ -125,9 +125,9 @@ def main():
     filtered_data = filter_outliers(data_loader, 5.0)
     plot_coordinates(filtered_data)
     # implement a path pruning algorithm minimize the points used
-    plot_coordinates(DouglasPeucker(filtered_data,1.0))
+    plot_coordinates(DouglasPeucker(filtered_data, 1.0))
     # convert back to lat lon
-    
+
     # Create a mission plan
 
 
