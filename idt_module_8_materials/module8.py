769caafe287a4d28da5d783d15f4ebb5eb9f4ea0
kasperg3 <kasperg3@rocketmail.com>
2022-11-24 13:35:07 +0100

    rssi

--- idt_module_8_materials/module8.py
+++ idt_module_8_materials/module8.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
 C = 299792458 # Speed of light m/s
 Cc = C * (2/3)
 frequency = 1000000 * 434
-wavelength = C / frequency
+wavelength = Cc / frequency
 antenna_legth = 0.5*wavelength
 
+print("Antenna length", wavelength)
 print("Antenna length", antenna_legth)
 print("dipole length", antenna_legth/2)
 
